5427ae5b00197ae10a85fae4f722b66cf35feaf6
FranzDerKanns <franz.schwinn@gibmit.ch>
2017-06-28 08:17:16 +0200

    Update DezBin.java

--- src/logic/DezBin.java
+++ src/logic/DezBin.java
@@ -1,9 +1,9 @@
 package logic;
 
 public class DezBin implements Calculation {
-
+//Verwendet das Interface "Calculation"
 	public String getoutput(String input) {
-
+//Gibt das Resultat der Rechnung als Rueckgabe zurueck
 		return Integer.toString(Integer.parseInt(input), 2); // decimal to
 																// binary
 
