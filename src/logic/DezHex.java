acbafc39bfd3494035c72ece4cb502994c7c3985
FranzDerKanns <franz.schwinn@gibmit.ch>
2017-06-28 08:18:15 +0200

    Update DezHex.java

--- src/logic/DezHex.java
+++ src/logic/DezHex.java
@@ -1,7 +1,7 @@
 package logic;
-
+//Verwendet das Interface "Calculation"
 public class DezHex implements Calculation {
-
+//Gibt Resultat der Rechnung als Rueckgabewert zurueck
 	public String getoutput(String input) {
 		return Integer.toString(Integer.parseInt(input), 16);
 	}
