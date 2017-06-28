73bc2753ac0a8891b85e601e1f433bbfe6619820
FranzDerKanns <franz.schwinn@gibmit.ch>
2017-06-28 08:16:35 +0200

    Update BinHex.java

--- src/logic/BinHex.java
+++ src/logic/BinHex.java
@@ -1,7 +1,7 @@
 package logic;
-
+//Verwendet das Interface "Calculation"
 public class BinHex implements Calculation {
-
+//Gibt das Resultat der Rechnung als Rueckgabewert zurueck
 	public String getoutput(String input) {
 		int decimal = Integer.parseInt(input, 2);
 		return Integer.toString(decimal, 16);
