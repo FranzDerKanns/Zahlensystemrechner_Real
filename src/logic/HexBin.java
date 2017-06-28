5ac7b83e85a4de4780bfd55f9534a0185fe3e06b
FranzDerKanns <franz.schwinn@gibmit.ch>
2017-06-28 08:19:24 +0200

    Update HexBin.java

--- src/logic/HexBin.java
+++ src/logic/HexBin.java
@@ -1,9 +1,9 @@
 package logic;
 
 import java.math.BigInteger;
-
+//Verwendet das Interface "Calculation"
 public class HexBin implements Calculation {
-
+//Gibt Resultat der Rechnung als Rueckgabewert zurueck
 	public String getoutput(String input) {
 		return new BigInteger(input, 16).toString(2);
 	}
