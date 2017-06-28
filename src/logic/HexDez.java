024e3e08634145554a091fdf3d5aa5fd23d13b8e
FranzDerKanns <franz.schwinn@gibmit.ch>
2017-06-28 08:20:16 +0200

    Update HexDez.java

--- src/logic/HexDez.java
+++ src/logic/HexDez.java
@@ -1,7 +1,7 @@
 package logic;
-
+//Verwendet das Interface "Calculation"
 public class HexDez implements Calculation {
-
+//Gibt Resultat der Rechnung als Rueckgabewert zurueck
 	public String getoutput(String input) {
 		return Integer.toString(Integer.valueOf(input, 16));
 	}
