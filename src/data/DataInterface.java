e5246ac67d7ab026ca070ff40d5c103543c3a50c
FranzDerKanns <franz.schwinn@gibmit.ch>
2017-06-28 08:09:17 +0200

    Update DataInterface.java

--- src/data/DataInterface.java
+++ src/data/DataInterface.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
+//Datainterface (Data-Schicht)
 public class DataInterface {
 	String history;
-
+//FÃ¼gt Input und Format des Inputs in ein Textfile
 	public void add(String input, String format) {
 		try {
 			PrintWriter writer = new PrintWriter("file.txt", "UTF-8");
@@ -23,16 +23,16 @@ public class DataInterface {
 
 			writer.close();
 		} catch (IOException e) {
-			// hier könnte Fehlermeldung stehen
+			// hier kÃ¶nnte Fehlermeldung stehen
 		}
 	}
-
+//Textfile wird ausgelesen, damit die Benutzereingabe ausgelesen werden kann
 	public static String gethistory() {
 		String text = "";
 		try {
 			text = new String(Files.readAllBytes(Paths.get("file.txt")));
 		} catch (IOException e) {
-			// hier könnte Fehlermeldung stehen
+			// hier kÃ¶nnte Fehlermeldung stehen
 		}
 
 		File file = new File("file.txt");
