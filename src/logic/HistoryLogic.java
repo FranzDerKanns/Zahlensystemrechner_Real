ac864889999805cc90b83bf76268105e8f62af75
FranzDerKanns <franz.schwinn@gibmit.ch>
2017-06-28 08:25:41 +0200

    Update HistoryLogic.java

--- src/logic/HistoryLogic.java
+++ src/logic/HistoryLogic.java
@@ -3,11 +3,11 @@ package logic;
 import java.util.ArrayList;
 
 import data.DataInterface;
-
+//Schnittstelle zwischen Gui-Schicht und Data-Schicht
 public class HistoryLogic {
 
 	DataInterface d = new DataInterface();
-
+//Holt das Resultat aus der Data-Schicht und stellt es via Getter an die Gui-Schicht zur Verfuegung
 	public String gethistory() {
 		return d.gethistory();
 	}
